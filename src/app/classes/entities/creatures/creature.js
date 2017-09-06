c34585444af5191d55076087184ce45b227ce00c
Matthew McCord <mattm.eternal@gmail.com>
2017-09-06 09:04:58 -0500

    cleanup collision for tiles with ternary

--- src/app/classes/entities/creatures/creature.js
+++ src/app/classes/entities/creatures/creature.js
@@ -48,27 +48,22 @@ export class Creature extends Entity {
       return;
     }
 
-    let tempX;
-    if (this.xMove > 0) {
-      tempX = parseInt((this.x + this.xMove + this.bounds.x + this.bounds.width) / TILE_WIDTH);
-      const c1 = parseInt((this.y + this.bounds.y) / TILE_HEIGHT);
-      const c2 = parseInt((this.y + this.bounds.y + this.bounds.height) / TILE_HEIGHT);
+    const tempX = this.xMove > 0
+      ? parseInt((this.x + this.xMove + this.bounds.x + this.bounds.width) / TILE_WIDTH)
+      : parseInt((this.x + this.xMove + this.bounds.x) / TILE_WIDTH);
+
+    const c1 = parseInt((this.y + this.bounds.y) / TILE_HEIGHT);
+    const c2 = parseInt((this.y + this.bounds.y + this.bounds.height) / TILE_HEIGHT);
+
+    const setX = this.xMove > 0
+      ? tempX * TILE_WIDTH - this.bounds.x - this.bounds.width - 1
+      : tempX * TILE_WIDTH + TILE_WIDTH - this.bounds.x;
+
       if(!this.collisionWithTile(tempX, c1) && !this.collisionWithTile(tempX, c2)) {
-        this.x += this.xMove;
-      } else {
-        this.x = tempX * TILE_WIDTH - this.bounds.x - this.bounds.width - 1;
-        if (this.type === 'player') this.checkForSwitchOrTrigger(c1, c2, tempX);
-      }
-    } else if (this.xMove < 0) {
-      tempX = parseInt((this.x + this.xMove + this.bounds.x) / TILE_WIDTH);
-      const c1 = parseInt((this.y + this.bounds.y) / TILE_HEIGHT);
-      const c2 = parseInt((this.y + this.bounds.y + this.bounds.height) / TILE_HEIGHT);
-      if(!this.collisionWithTile(tempX, c1) && !this.collisionWithTile(tempX, c2)) {
-        this.x += this.xMove;
-      } else {
-        this.x = tempX * TILE_WIDTH + TILE_WIDTH - this.bounds.x;
-        if (this.type === 'player') this.checkForSwitchOrTrigger(c1, c2, tempX);
-      }
+      this.x += this.xMove;
+    } else {
+      this.x = setX;
+      if (this.type === 'player') this.checkForSwitchOrTrigger(c1, c2, tempX);
     }
   }
 
@@ -78,29 +73,22 @@ export class Creature extends Entity {
       return;
     }
 
-    let tempY;
-    if (this.yMove > 0) {
-      tempY = parseInt((this.y + this.yMove + this.bounds.y + this.bounds.height) / TILE_HEIGHT);
-      const c1 = parseInt((this.x + this.bounds.x) / TILE_WIDTH);
-      const c2 = parseInt((this.x + this.bounds.x + this.bounds.width) / TILE_WIDTH);
-      if(!this.collisionWithTile(c1, tempY) &&
-        !this.collisionWithTile(c2, tempY)) {
-        this.y += this.yMove;
-      } else {
-        this.y = tempY * TILE_HEIGHT - this.bounds.y - this.bounds.height - 1;
-        this.checkForSwitchOrTrigger(c1, c2, tempY);
-      }
-    } else if (this.yMove < 0) {
-      tempY = parseInt((this.y + this.yMove + this.bounds.y) / TILE_HEIGHT);
-      const c1 = parseInt((this.x + this.bounds.x) / TILE_WIDTH);
-      const c2 = parseInt((this.x + this.bounds.x + this.bounds.width) / TILE_WIDTH);
-      if(!this.collisionWithTile(c1, tempY) &&
-        !this.collisionWithTile(c2, tempY)) {
-        this.y += this.yMove;
-      } else {
-        this.y = tempY * TILE_HEIGHT + TILE_HEIGHT - this.bounds.y;
-        this.checkForSwitchOrTrigger(c1, c2, tempY);
-      }
+    const tempY = this.yMove > 0
+      ? parseInt((this.y + this.yMove + this.bounds.y + this.bounds.height) / TILE_HEIGHT)
+      : parseInt((this.y + this.yMove + this.bounds.y) / TILE_HEIGHT);
+
+    const c1 = parseInt((this.x + this.bounds.x) / TILE_WIDTH);
+    const c2 = parseInt((this.x + this.bounds.x + this.bounds.width) / TILE_WIDTH);
+
+    const setY = this.yMove > 0
+      ? tempY * TILE_HEIGHT - this.bounds.y - this.bounds.height - 1
+      : tempY * TILE_HEIGHT + TILE_HEIGHT - this.bounds.y;
+
+    if (!this.collisionWithTile(c1, tempY) && !this.collisionWithTile(c2, tempY)) {
+      this.y += this.yMove;
+    } else {
+      this.y = setY;
+      if (this.type === 'player') this.checkForSwitchOrTrigger(c1, c2, tempY);
     }
   }
 
@@ -108,7 +96,7 @@ export class Creature extends Entity {
     const tile1 = this.handler.getWorld().getTile(c1, temp);
     const tile2 = this.handler.getWorld().getTile(c2, temp);
 
-    // console.log(tile1.type, tile2.type);
+    console.log({checking: '', tile1, tile2})
 
     if (tile1.type === 'switch') {
       this.handler.getWorld().swapGreenAndBlueTiles(tile1.color);
@@ -117,10 +105,6 @@ export class Creature extends Entity {
     }
 
     if (tile1.type === 'exit' || tile2.type === 'exit') {
-      console.log('EXIT HIT')
-      console.log('EXIT HIT')
-      console.log('EXIT HIT')
-      console.log('EXIT HIT')
       if (this.handler.getWorld().level >= 4) {
         const ending = new Ending(this.handler);
         this.handler.getGame().getGameState().setState(ending);
