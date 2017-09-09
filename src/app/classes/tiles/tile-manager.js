eef1ee10f562f1b81f443469ee8338f63c1afdd2
Matthew McCord <mattm.eternal@gmail.com>
2017-09-09 18:22:30 -0500

    cleaning up to reduce file size

--- src/app/classes/tiles/tile-manager.js
+++ src/app/classes/tiles/tile-manager.js
@@ -1,15 +1,12 @@
 import { Assets } from '../gfx/assets';
-import { Exit } from './triggers/exit';
 import { Path } from './paths/path';
 import { PathBlue } from './paths/path-blue';
 import { PathYellow } from './paths/path-yellow';
 import { Wall } from './walls/wall';
 import { WallBlue } from './walls/wall-blue';
 import { WallYellow } from './walls/wall-yellow';
-import { SwitchBlue } from './switches/switch-blue';
 import { PathGreen } from './paths/path-green';
 import { WallGreen } from './walls/wall-green';
-import { SwitchGreen } from './switches/switch-green';
 
 const assets = Assets.getAssets("tiles");
 const tiles = [];
@@ -20,11 +17,8 @@ tiles[2] = new WallYellow(2);
 tiles[3] = new WallBlue(3);
 tiles[4] = new PathYellow(4);
 tiles[5] = new PathBlue(5);
-// tiles[6] = new SwitchBlue(6);
 tiles[7] = new PathGreen(7);
 tiles[8] = new WallGreen(8);
-// tiles[9] = new SwitchGreen(9);
-// tiles[10] = new Exit(10);
 
  export class TileManager {
 
