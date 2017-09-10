07aeca6880ee1e58b726ede25e1d38296c36ae5c
Matthew McCord <mattm.eternal@gmail.com>
2017-09-09 20:07:44 -0500

    trying to configure webpack on windows is stupid

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const isProduction = process.env.npm_lifecycle_event === 'build';
 
@@ -62,7 +63,13 @@ if(!isProduction) {
   config.devtool = 'eval-source-map'
 } else {
   config.plugins = config.plugins.concat([
-    new webpack.optimize.ModuleConcatenationPlugin()
+		new CompressionPlugin({
+			asset: "[path].gz[query]",
+			algorithm: "gzip",
+			test: /\.js$|\.css$|\.html$/,
+			threshold: 10240,
+			minRatio: 0.8
+		})
   ])
 }
 
