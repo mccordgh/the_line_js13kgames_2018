09e79ff31d3426ea7cb50c1d78137b1ceb8e9eb2
Matthew McCord <mattm.eternal@gmail.com>
2018-08-23 22:34:12 -0500

    Refactor assets loading

--- src/app/classes/gfx/assets.js
+++ src/app/classes/gfx/assets.js
@@ -1,9 +1,8 @@
-// import { Animation } from './animation';
+import { Animation } from './animation';
 import { ImageLoader } from './image-loader';
 import { SpriteSheet } from './sprite-sheet';
 
 let assets = {}, PATH  = window.location.href.replace('index.html', '') + 'src/res/';
-// let SPRITE_SIZE = 16, SPRITE_SIZE = 16;
 
 export class Assets {
   constructor(name, path) {
@@ -13,6 +12,11 @@ export class Assets {
     this.width = SPRITE_SIZE;
     this.height = SPRITE_SIZE;
     this.sheet = new SpriteSheet(ImageLoader.loadImage(this.path));
+    this.anim = {};
+  }
+
+  addAnimation(name, animation) {
+    this.anim[name] = animation;
   }
 
   static getAssets(name) {
@@ -22,18 +26,41 @@ export class Assets {
 
 let cropTile = (asset, x, y) => asset.sheet.crop(SPRITE_SIZE * x, SPRITE_SIZE * y, SPRITE_SIZE, SPRITE_SIZE);
 
+let addAnimations = (sprites, name, right, left, multi = true) => {
+  let speed = 300, wrframes = [], wlframes = [];
+  let cropFrame = (pos, i) => {
+    return sprites.sheet.crop(SPRITE_SIZE * i, SPRITE_SIZE * pos, SPRITE_SIZE, SPRITE_SIZE);
+  }
+
+  for (let i = 0; i < 2; i++) {
+    wrframes.push({
+      frame: cropFrame(right, i),
+      speed
+    });
+    if (multi) {
+      wlframes.push({
+        frame: cropFrame(left, i),
+        speed
+      });
+    }
+  }
+
+  sprites.addAnimation(name + "right", new Animation(wrframes));
+  sprites.addAnimation(name + "left", new Animation(wlframes));
+};
+
 /* TILES */
 /* WALL */
-let wall = new Assets('wall', PATH + 'wall.png');
-wall.wall = cropTile(wall, 0, 0);
+// let wall = new Assets('wall', PATH + 'wall.png');
+// wall.wall = cropTile(wall, 0, 0);
 
 /* PATH */
-let path = new Assets('path', PATH + 'path.png');
-path.path = cropTile(path, 0, 0);
+// let path = new Assets('path', PATH + 'path.png');
+// path.path = cropTile(path, 0, 0);
 
 /* PLAYER */
-let player = new Assets('player', PATH + 'worker_3.png');
-player.idle = cropTile(player, 0, 0);
+// let player = new Assets('player', PATH + 'worker_3.png');
+// player.idle = cropTile(player, 0, 0);
 
 // let tiles = new Assets('tiles', PATH + "all_tiles.png", SPRITE_SIZE, SPRITE_SIZE);
 // tiles.path = cT(tiles, 1, 0);
@@ -43,33 +70,8 @@ player.idle = cropTile(player, 0, 0);
 // tiles.exit = cT(tiles, 1, 2);
 // tiles.lantern = cT(tiles, 0, 2);
 
-// let aA = (sprites, n, wurow, wrrow, wdrow, wlrow) => {
-//   let framespeed = 200, wrframes = [], wlframes = [], wuframes = [], wdframes = [];
 
-//   for (let i = 0; i < 2; i++) {
-//     wuframes.push({
-//       frame: sprites.sheet.crop(sprites.width * i, sprites.height * wurow, sprites.width, sprites.height),
-//       speed: framespeed
-//     });
-//     wrframes.push({
-//       frame: sprites.sheet.crop(sprites.width * i, sprites.height * wrrow + 1, sprites.width, sprites.height),
-//       speed: framespeed
-//     });
-//     wdframes.push({
-//       frame: sprites.sheet.crop(sprites.width * i, sprites.height * wdrow + 1, sprites.width, sprites.height),
-//       speed: framespeed
-//     });
-//     wlframes.push({
-//       frame: sprites.sheet.crop(sprites.width * i, sprites.height * wlrow + 1, sprites.width, sprites.height),
-//       speed: framespeed
-//     });
-//   }
-
-//   sprites.aA(n + "walk_up", new Animation(wuframes));
-//   sprites.aA(n + "walk_right", new Animation(wrframes));
-//   sprites.aA(n + "walk_down", new Animation(wdframes));
-//   sprites.aA(n + "walk_left", new Animation(wlframes));
-// };
-
-// aA(tiles, 'p', 3, 4, 5, 6);
-// aA(tiles, 'c', 7, 8, 9, 10);
+let all = new Assets('all', PATH + 'all.png');
+addAnimations(all, 'p', 0, 1);
+addAnimations(all, 'g', 2, 3);
+addAnimations(all, 'w', 4, 0, false)
