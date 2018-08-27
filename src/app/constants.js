811c043468eb147c8fa34aab623796f8e491779e
Matthew McCord <matthewmccord@matthews-mbp.attlocal.net>
2018-08-27 09:14:03 -0500

    Add global method for random array index

--- src/app/constants.js
+++ src/app/constants.js
@@ -5,4 +5,5 @@ window.SPRITE_SIZE = 8; //px
 window.TILE_COUNT = 12; //tiles
 window.TILE_SIZE = 64; //px
 
+window.rndIndex = (nums) => nums[Math.floor(Math.random() * (nums.length))];
 window.rndInt = (nums) => nums[Math.floor(Math.random() * (nums.length))];
